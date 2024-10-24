2fc2bc404ac5bdc7636aa15e33e48ea01c4575a7
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-10-24 11:37:24 +0100

    Updating model to work for cumulative prob distribution

--- simplistic_bayesian_model.py
+++ simplistic_bayesian_model.py
@@ -1,6 +1,7 @@
 # %%
 from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
 
 # %%
 ## Network with three edges, each of which influence lock-in
@@ -19,36 +20,28 @@ model = BayesianNetwork(
 cpd_AGI_means = TabularCPD(
     variable="AGI",
     variable_card=5,
-    values=[[0.2809, 0.6433, 0.7649, 0.8673, 0.9250]],
+    values=[[0.2809], [0.6433], [0.7649], [0.8673], [0.9250]],
 )
 
 cpd_misalignment = TabularCPD(
     variable="misalignment",
-    variable_card=2,
+    variable_card=1,
     values=[[0.5293]],
 )
 
 cpd_wwiii = TabularCPD(
     variable="world_war_iii",
     variable_card=5,
-    values=[[0.3, 0.3144, 0.3861, 0.4579, 0.5297]],
+    values=[[0.3], [0.3144], [0.3861], [0.4579], [0.5297]],
 )
 
 # %%
-## Conditional probability distributions for lock-in
-cpd_lock_in = TabularCPD(
-    variable="lock_in",
-    variable_card=2,
-    values=[
-        [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
-        [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0],
-    ],
-    evidence=["world_war_iii", "AGI", "misalignment"],
-    evidence_card=[5, 5, 2],
-)
+## Variable elimination
+model.add_cpds(cpd_AGI_means, cpd_misalignment, cpd_wwiii)
 
-# %%
-model.add_cpds(cpd_AGI_means, cpd_misalignment, cpd_wwiii, cpd_lock_in)
+inference = VariableElimination(model)
 
-# %%
-print(model.check_model())
+p_lock_in = inference.query(["lock_in"])
+
+print("Probability distribution of lock-in:")
+print(p_lock_in)
